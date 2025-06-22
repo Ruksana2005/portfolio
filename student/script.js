87587fd1671514b7cd83f4854a225b026c401747
Ruksana2005 <143307977+Ruksana2005@users.noreply.github.com>
2025-06-22 17:05:03 +0530

    Delete student directory

--- student/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    const errorMessage = document.getElementById('error-message');
-    errorMessage.classList.add('hidden');
-});
